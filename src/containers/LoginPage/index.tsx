d79457978a0669c8d93991ac650b21e0c3af1307
Nguyen_Thanh_Se <thanhse123@gmail.com>
2021-04-21 21:48:48 +0700

    update-redux

--- src/containers/LoginPage/index.tsx
+++ src/containers/LoginPage/index.tsx
@@ -1,9 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link, RouteComponentProps } from "react-router-dom";
 import LoginForm from './../../component/LoginForm';
+import { Dispatch } from "redux"
+import { useSelector, shallowEqual, useDispatch } from "react-redux"
+import { addArticle, removeArticle } from "./../../store/actionCreators";
+import './styles.scss';
 function Login() {
-  return (<div style={{width:'100%',height:'100vh',display:'flex',alignItems:'center',justifyContent:'center',backgroundColor:'#67ebdb'}}>
-    <div style={{width:410,borderRadius:8,border:'1px solid #bfbdbd', padding:'20px 0px 10px 0px',backgroundColor:'white'}}>
+  const articles: readonly IArticle[] = useSelector(
+    (state: ArticleState) => state.articles,
+    shallowEqual
+  )
+
+  // const dispatch: Dispatch<any> = useDispatch()
+
+  // const saveArticle = React.useCallback(
+  //   (article: IArticle) => dispatch(addArticle(article)),
+  //   [dispatch]
+  // )
+  console.log('state',articles);
+  return (<div className="login">
+    <div className="card_login">
     <LoginForm></LoginForm>
     </div>
   </div>);
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware, Store } from "redux"
+import { Provider } from "react-redux"
+import thunk from "redux-thunk"
 import 'antd/dist/antd.css';
+import reducer from "./store/reducer"
+const store: Store<ArticleState, ArticleAction> & {
+  dispatch: DispatchType
+} = createStore(reducer,applyMiddleware(thunk))
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+     <Provider store={store}>
+     <App />
+     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
